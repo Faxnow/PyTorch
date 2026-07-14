bedfaf884ee2c5bde200d3734333bb4dbf0e740b
Faxnow <olgakrylova98967@gmail.com>
2026-07-14 19:51:31 +0300

    Add files via upload

--- torch_cnn.py
+++ torch_cnn.py
@@ -10,21 +10,21 @@ class CNN(nn.Module):
     def __init__(self, in_channel = 1, num_classes = 10):
         super(CNN, self).__init__()
         self.conv1 = nn.Conv2d(in_channels=1, out_channels=8, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
-        self.conv1 = nn.Conv2d(in_channels=1, out_channels=8, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
+        self.conv2 = nn.Conv2d(in_channels=8, out_channels=16, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
         self.pool = nn.MaxPool2d(kernel_size=(2, 2), stride=(2, 2))
         self.fc1 = nn.Linear(16*7*7, num_classes)
 
     def forward(self, x):
         x = F.relu(self.conv1(x))
         x = self.pool(x)
-        x = F.relu(self.fc2(x))
+        x = F.relu(self.conv2(x))
         x = self.pool(x)
         x = x.reshape(x.shape[0], -1)
         x = self.fc1(x)
 
         return x
-# model = CNN()
-# x = torch.randn(64, 1, 28, 28)
+model = CNN()
+x = torch.randn(64, 1, 28, 28)
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 
@@ -40,7 +40,7 @@ test_dataset = datasets.MNIST(root='.data', train=False, transform=transforms.To
 train_loader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
 test_loader = DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=True)
 
-model = CNN(input_size= input_size, num_classes= num_classes).to(device)
+model = CNN(in_channel=in_channel, num_classes=num_classes).to(device)
 
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.Adam(model.parameters(), lr=learning_rate)
